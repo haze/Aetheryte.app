62a7dc3bac34621ea53fb57b1e8c5593d662540a
Haze Booth <isnt@haze.cool>
2025-08-05 02:33:06 -0400

    Initial Commit

---
+++ Aetheryte/AetheryteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AetheryteApp.swift
+//  Aetheryte
+//
+//  Created by Haze Booth on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AetheryteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aetheryte/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aetheryte
+//
+//  Created by Haze Booth on 8/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
